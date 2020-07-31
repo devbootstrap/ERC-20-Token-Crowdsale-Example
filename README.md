f014e598d9d2050280e906a6eb26c27db225387f
Darren Jensen <darren.jensen@gmail.com>
2020-07-31 18:59:43 +0700

    added some ERC-20 contract specific tests

---
+++ README.md
@@ -0,0 +1,27 @@
+# Example ERC-20 Token and Crowdsale Smart Contract using OpenZeppelin 2x
+
+## Test run the Smart Contracts!
+
+To test run these Smart Contracts you will need to have truffle installed and then in a new terminal window start up `Ganache`:
+
+```
+ganache-cli
+```
+
+Then in a *new terminal window* run these `truffle` commands:
+
+```
+truffle compile
+truffle migrate --reset
+truffle test
+```
+
+## Libraries used / tested against
+
+* Truffle v5.1.14-nodeLTS.0 (core: 5.1.13)
+* Solidity v0.5.16 (solc-js)
+* Node v12.18.1
+* Web3.js v1.2.1
+* @openzeppelin/contracts@2.5.1
+
+t
--- test/GLDTokenTest.js
+++ test/GLDTokenTest.js
@@ -1,6 +1,8 @@
 var GLDToken = artifacts.require("GLDToken");
+const truffleAssert = require('truffle-assertions');
 
 contract('GLDToken', (accounts) => {
+    const emptyAddress = '0x0000000000000000000000000000000000000000'
     var creatorAddress = accounts[0];
     var firstOwnerAddress = accounts[1];
     var secondOwnerAddress = accounts[2];
@@ -12,7 +14,7 @@ contract('GLDToken', (accounts) => {
         instance = await GLDToken.deployed();
     })
 
-    describe('token name and symbol metadata', () => {
+    describe('token name, symbol and total supply metadata', () => {
         it('name() should return the token name', async () => {
             // let instance = await GLDToken.deployed();
             let name = await instance.name();
@@ -22,6 +24,10 @@ contract('GLDToken', (accounts) => {
             // let instance = await GLDToken.deployed();
             let symbol = await instance.symbol();
             assert.equal(symbol, 'GLD')
+        })
+        it('should have a totalsupply as expected', async ()=>{
+            let ts = await instance.totalSupply()
+            assert.equal(ts.toString(), '100000', 'Unexpected Total Supply')
         })
     })
 
@@ -39,6 +45,20 @@ contract('GLDToken', (accounts) => {
             let creatorBal = await instance.balanceOf(creatorAddress)
             assert.equal(firstOwnerBal.toString(), '300', 'First owner did not receive transfer')
             assert.equal(creatorBal.toString(), '99700', 'First owner did not receive transfer')
+        })
+        it('should emit a Transfer event', async () => {
+            let res = await instance.transfer(firstOwnerAddress, 300)
+            truffleAssert.eventEmitted(res, 'Transfer', (e) => {
+                return  e.from == creatorAddress &&
+                        e.to == firstOwnerAddress &&
+                        e.value == 300
+            })
+        })
+        it('should fail if the recipient address is a 0x0 address', async () => {
+            await truffleAssert.reverts(
+                instance.transfer(emptyAddress, 300),
+                "ERC20: transfer to the zero address"
+            );
         })
     })
 });
